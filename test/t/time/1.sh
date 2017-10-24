6e3c24bcb387182a9e7aa498b75c8487c83d65ae
Anton Kozlov <drakon.mega@gmail.com>
2017-10-25 01:11:49 +0300

    Extend time test as supposed in #28

--- test/t/time/1.sh
+++ test/t/time/1.sh
@@ -1,3 +1,4 @@
+echo -n "> "
 date +%s.%N
 sleep 2
 date +%s.%N
--- test/t/time/run.sh
+++ test/t/time/run.sh
@@ -5,9 +5,15 @@
 make_eduos
 
 check() {
-	time $1 | \
-		awk 'NR == 1 { l = $1 } NR == 2 { if ($1 - l >= 2.0) print "OK" }' | \
+	t1=`date +%s.%N`
+	$1 | \
+		awk 'NR == 1 { l = $2 } NR == 2 { if ($1 - l >= 2.0) print "OK" }' | \
 		grep "OK"
+	r1=$?
+	t2=`date +%s.%N`
+	echo "$t1 $t2" | awk '{if ($2-$1 >= 2.0) print "OK"}' | grep "OK"
+	r2=$?
+	[ $r1 = 0 ] && [ $r2 = 0 ]
 }
 
 #check /bin/sh < $CDIR/1.sh 
