45fe27e341cb872adf017b84ecc47c6cde859d23
VladimirMaltsev <vov.maltsev@gmail.com>
2017-10-30 16:53:15 +0300

    init file system

--- src/os.c
+++ src/os.c
@@ -6,10 +6,12 @@
 #include "os/irq.h"
 #include "os/sched.h"
 #include "os/time.h"
+#include "os/filesys.h"
 
 #include "apps.h"
 
 int main(int argc, char *argv[]) {
+	
 	irq_init();
 
 	syscall_init();
@@ -17,6 +19,8 @@ int main(int argc, char *argv[]) {
 	time_init();
 
 	sched_init();
+
+	argc > 1 ? filesys_init(argv[1]) : filesys_init("");
 
 	sched_add(shell, NULL);
 
